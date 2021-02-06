22f614bc9235ec20a7484570a17a8ec2720d2e86
Omniguedes <desguedes@gmail.com>
2021-02-06 19:16:12 +0000

    basic start background

--- src/co/weirddoeats/attributes/Background.java
+++ src/co/weirddoeats/attributes/Background.java
@@ -1,4 +1,22 @@
 package co.weirddoeats.attributes;
 
+import co.weirddoeats.gfx.simplegfx.SimpleGfxGridPosition;
+import co.weirddoeats.grid.position.GridPosition;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class Background {
+
+    private Picture pic;
+    private SimpleGfxGridPosition pos;
+    private final int PADDING = 10;
+
+    public Background(String pic){
+        this.pic = new Picture(PADDING, PADDING, pic);
+    }
+    public void show(){
+        pic.draw();
+    }
+    public void hide(){
+        pic.delete();
+    }
 }
