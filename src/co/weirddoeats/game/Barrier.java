adb51eecc03de34ab110fd6e0684af38b0c6ba34
Omniguedes <desguedes@gmail.com>
2021-02-10 20:09:40 +0000

    first implementations on barrier

--- src/co/weirddoeats/game/Barrier.java
+++ src/co/weirddoeats/game/Barrier.java
@@ -1,5 +1,34 @@
 package co.weirddoeats.game;
 
+import co.weirddoeats.gfx.simplegfx.GameGrid;
+import co.weirddoeats.gfx.simplegfx.SimpleGfxGridPosition;
+
 public class Barrier {
+
+    private SimpleGfxGridPosition position;
+    private boolean horizontal;
+    private int col;
+    private int row;
+
+    public Barrier(){
+        plane();
+    }
+
+    public boolean isHorizontal(){
+        return horizontal;
+    }
+
+    public void generatePositions(GameGrid grid) {
+        position = grid.makeGridPosition();
+    }
+
+    public boolean plane(){
+        if(0 == (int) Math.floor(Math.random()* 2)){
+            horizontal = true;
+        }else{
+            horizontal = false;
+        }
+        return horizontal;
+    }
 
 }
